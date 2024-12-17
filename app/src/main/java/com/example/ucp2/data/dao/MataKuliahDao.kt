e901da582a1ea6466d79dd52cee2b2b27e42bbdc
PutriHabibahDevina <putrihdm13@gmail.com>
2024-12-18 03:43:55 +0700

    Membuat operasi create, read, update, delete, dan detail untuk tabel matakuliah

---
+++ app/src/main/java/com/example/ucp2/data/dao/MataKuliahDao.kt
@@ -0,0 +1,32 @@
+package com.example.ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MataKuliahDao {
+    //Create
+    @Insert
+    suspend fun insertMataKuliah(mataKuliah: MataKuliah)
+
+    //Read
+    @Query("SELECT * FROM matakuliah ORDER BY namaMK ASC ")
+    fun getAllMataKuliah(): Flow<List<MataKuliah>>
+
+    //Update
+    @Update
+    suspend fun updateMataKuliah(mataKuliah: MataKuliah)
+
+    //Delete
+    @Delete
+    suspend fun deleteMataKuliah(mataKuliah: MataKuliah)
+
+    //Detail
+    @Query("SELECT * FROM matakuliah WHERE kode = :kode ")
+    fun getMataKuliah(kode: String): Flow<MataKuliah>
+}
