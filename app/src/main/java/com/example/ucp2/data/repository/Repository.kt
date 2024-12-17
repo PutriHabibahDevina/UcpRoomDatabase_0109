0c69f111518a34aa2b528cd9fef6f41ac5bb5655
PutriHabibahDevina <putrihdm13@gmail.com>
2024-12-18 03:45:47 +0700

    Menambahkan repository dosen

--- app/src/main/java/com/example/ucp2/data/repository/Repository.kt
+++ app/src/main/java/com/example/ucp2/data/repository/Repository.kt
@@ -1,12 +1,16 @@
 package com.example.ucp2.data.repository
 
+import com.example.ucp2.data.entity.Dosen
 import com.example.ucp2.data.entity.MataKuliah
 import kotlinx.coroutines.flow.Flow
 
-interface RepositoryMK {
+interface Repository {
     suspend fun insertMK(mataKuliah: MataKuliah)
     fun getAllMataKuliah() : Flow<List<MataKuliah>>
     suspend fun updateMK(mataKuliah: MataKuliah)
     suspend fun deleteMK(mataKuliah: MataKuliah)
-    fun getDetailMataKuliah(kode: String): Flow<MataKuliah?>
+    fun getMK(kode: String): Flow<MataKuliah?>
+
+    suspend fun insertDosen(dosen: Dosen)
+    fun getAllDosen() : Flow<List<Dosen>>
 }
