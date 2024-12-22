5741a789c790c19f219907308a21be854edfc0af
PutriHabibahDevina <putrihdm13@gmail.com>
2024-12-22 12:43:59 +0700

    Membuat dpenyedia view model

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.ucp2.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.praktikum7.ui.viewmodel.HomeDosenViewModel
+import com.example.ucp2.KrsApp
+import com.example.ucp2.ui.viewmodel.dosen.DetailViewModel
+import com.example.ucp2.ui.viewmodel.dosen.DosenViewModel
+import com.example.ucp2.ui.viewmodel.matakuliah.MataKuliahViewModel
+import com.example.ucp2.ui.viewmodel.matakuliah.UpdateMKViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            DosenViewModel(
+                krsApp().containerApp.repository
+            )
+            MataKuliahViewModel(
+                krsApp().containerApp.repository
+            )
+        }
+
+        initializer {
+            HomeDosenViewModel(
+                krsApp().containerApp.repository
+            )
+            HomeMKViewModel(
+                krsApp().containerApp.repository
+            )
+        }
+
+        initializer {
+            DetailDosenViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repository
+            )
+            DetailMKViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repository
+            )
+        }
+
+        initializer {
+            UpdateMKViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repository
+            )
+        }
+    }
+}
+
+fun CreationExtras.krsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
