14b58d292d53c2135085f628100c7765f8cff571
PutriHabibahDevina <putrihdm13@gmail.com>
2024-12-22 12:43:09 +0700

    Membuat view model dan detail view model untuk dosen

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/DetailDosenViewModel.kt
@@ -0,0 +1,73 @@
+package com.example.ucp2.ui.viewmodel.dosen
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.data.repository.Repository
+import com.example.ucp2.ui.navigation.DestinasiDosenDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class DetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repository: Repository
+) : ViewModel(){
+    private val _nidn: String = checkNotNull(savedStateHandle[DestinasiDosenDetail.NIDN])
+    val detailUiState: StateFlow<DetailUiState> = repository.getDosen(_nidn)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            )
+        )
+}
+
+data class DetailUiState(
+    val detailUiEvent: DosenEvent = DosenEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == DosenEvent()
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != DosenEvent()
+}
+
+/*Data class untuk menampung data yang akan ditampilkan di UI*/
+//Memindahkan data dari entity ke ui
+fun Dosen.toDetailUiEvent(): DosenEvent {
+    return DosenEvent(
+        nidn = nidn,
+        nama = nama,
+        jenisKelamin = jenisKelamin
+    )
+}
---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/DosenViewModel.kt
@@ -0,0 +1,89 @@
+package com.example.ucp2.ui.viewmodel.dosen
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.data.repository.Repository
+import kotlinx.coroutines.launch
+
+class DosenViewModel(private val repository: Repository) : ViewModel()
+{
+    var uiState by mutableStateOf(DosenUIState())
+
+    //Memperbarui state berdasarkan input pengguna
+    fun updateState(dosenEvent: DosenEvent){
+        uiState = uiState.copy(
+            dosenEvent = dosenEvent,
+        )
+    }
+    //Validasi data input pengguna
+    private fun validateFields():Boolean {
+        val event = uiState.dosenEvent
+        val errorState = FormErrorState(
+            nidn = if (event.nidn.isNotEmpty()) null else "NIDN tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong"
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+    //Menyimpan data ke repository
+    fun saveData(){
+        val currentEvent = uiState.dosenEvent
+        if(validateFields()){ //Kalo validasinya bener
+            viewModelScope.launch {
+                try {
+                    repository.insertDosen(currentEvent.toDosenEntity())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data berhasil disimpan",
+                        dosenEvent = DosenEvent(), //Reset input form
+                        isEntryValid = FormErrorState() //Reset error state
+                    )
+                } catch (e:Exception){
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data gagal disimpan"
+                    )
+                }
+            }
+        }
+    }
+    //Reset pesan Snackbar setelah ditampilkan
+    fun resetSnackBarMessage(){
+        uiState = uiState.copy(snackBarMessage = null)
+    }
+}
+
+//Untuk menghandle perubahan, misal kaya error kan ntar tampilannya berubah jd error
+data class DosenUIState(
+    val dosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null //kayak pop up
+)
+
+//Untuk validasi
+data class FormErrorState(
+    val nidn: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null
+){
+    fun isValid():Boolean{
+        return nidn == null && nama == null && jenisKelamin == null
+    }
+}
+
+//Menyimpan input form ke dalam entity
+fun DosenEvent.toDosenEntity(): Dosen = Dosen( //Data yang diinputkan akan diimasukkan ke dalam Entity/entitas
+    nidn = nidn,
+    nama = nama,
+    jenisKelamin = jenisKelamin
+)
+
+//Data class variabel yang menyimpan data input form
+data class DosenEvent(
+    val nidn: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = ""
+)
